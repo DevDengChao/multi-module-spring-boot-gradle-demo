729d99649d997053e7b6d4c02097d7a87207e847
dengchao@xgtl <2325690622@qq.com>
2021-07-27 10:47:18 +0800

    测试 lib 模块中的类在运行时可以被访问

--- app/src/main/java/dev/dengchao/AppService.java
+++ app/src/main/java/dev/dengchao/AppService.java
@@ -1,7 +1,26 @@
 package dev.dengchao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppService {
+    private static final Logger log = LoggerFactory.getLogger(AppService.class);
+
+    @NonNull
+    private final LibComponent component;
+
+    public AppService(@NonNull LibComponent component) {
+        this.component = component;
+        log.info("Component injected {}", component);
+    }
+
+    @NonNull
+    public String hello(@NonNull String name) {
+        String res = component.hello(name);
+        log.info("{}", res);
+        return res;
+    }
 }
--- app/src/test/java/dev/dengchao/AppServiceTest.java
+++ app/src/test/java/dev/dengchao/AppServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
@@ -15,5 +16,7 @@ class AppServiceTest {
     @Test
     void test() {
         assertNotNull(service);
+
+        assertEquals("Hello World", service.hello("World"));
     }
 }
