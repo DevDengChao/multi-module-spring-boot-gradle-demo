a186b905c5e474201b4d26d02c8924bbd16c91fa
dengchao@xgtl <2325690622@qq.com>
2021-07-27 10:40:38 +0800

    测试 AppService

---
+++ app/src/test/java/dev/dengchao/AppServiceTest.java
@@ -0,0 +1,19 @@
+package dev.dengchao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class AppServiceTest {
+
+    @Autowired
+    private AppService service;
+
+    @Test
+    void test() {
+        assertNotNull(service);
+    }
+}
